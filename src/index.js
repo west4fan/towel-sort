efe724c87e8e2116e7d27a1e46559475aaa663e1
Kursky Ivan <iskurskiii@gmail.com>
2020-10-25 15:40:11 +0300

    feat: added functionality

--- src/index.js
+++ src/index.js
@@ -2,5 +2,13 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if ( !Array.isArray(matrix) || (matrix.length == 0) ) return ([]);
+  let sortedMatrix = [];
+
+    matrix.forEach((subMatrix, index) => { 
+      if (index % 2 !== 0) { sortedMatrix = sortedMatrix.concat( subMatrix.reverse() ); }
+      else { sortedMatrix = sortedMatrix.concat(subMatrix); }
+    });
+
+  return sortedMatrix;
 }
